08fea71d14d8992f570d5140a3d1babb299c7a59
Durga Sai Preetham <durgasaipreethamp@yahoo.com>
2016-12-14 16:08:47 -0800

    Basics of audio in android

--- app/src/main/java/com/yahoo/palagummi/sounddemo/MainActivity.java
+++ app/src/main/java/com/yahoo/palagummi/sounddemo/MainActivity.java
@@ -1,13 +1,27 @@
 package com.yahoo.palagummi.sounddemo;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    MediaPlayer mediaPlayer = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // get the audio file
+        mediaPlayer = MediaPlayer.create(this, R.raw.laugh);
+    }
+
+    public void startSound(View view) {
+        mediaPlayer.start();
+    }
+    public void stopSound(View view) {
+        mediaPlayer.pause();
     }
 }
